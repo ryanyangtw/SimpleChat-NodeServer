9a9f7a92a569b46b55d713a6c4b12c168b87d441
Ryan <hiphubryan@gmail.com>
2015-05-10 19:00:41 +0800

    Finish simple chat & create message & notification

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var request = require('request');
 
 // Initialize a new instance of socket.io by passing the http (the HTTP server) object
 var io = require('socket.io')(http);
@@ -13,6 +14,7 @@ var port = process.env.PORT || 5000; // Use the port that Heroku provides or def
 
 io.on('connection', function(socket) {
   console.log('a user connected');
+
 
   // io.emit = io.sockets.emit 會emit訊息給所有連上的socket
   //io.emit('message', 'Welcome to the most interesting');
@@ -33,23 +35,83 @@ io.on('connection', function(socket) {
   */
 
 
-  socket.on('join_room', function(room_number) {
+  socket.on('join_room', function(senderId, room_number) {
     socket.join(room_number);
 
-    console.log('a user join room number ' + room_number)
+    socket.senderId = senderId;
 
-    socket.room = room_number
-    io.sockets.in(room_number).emit('room_message', "Somebody join this fucking room. Room number: " + room_number)
-    //io.sockets.in(room_number).broadcast.emit('room_message', "Somebody join this fucking room. Room number: " + room_number)
+    console.log('a user join room number ' + room_number);
+
+    socket.room = room_number;
+    //io.sockets.in(room_number).emit('room_message', "100000", "Welcome come to room number: " + room_number)
+    socket.emit('room_message', "100000", 'Welcome to room number ' + room_number);
+    socket.broadcast.in(room_number).emit('room_message', "100000", "Somebody join this room. Room number: " + room_number);
+    //io.sockets.in(room_number).broadcast.emit('room_message', "100000", "Somebody join this room. Room number: " + room_number);
   });
 
-  socket.on('room_message', function(message) {
+  socket.on('room_message', function(senderId, message) {
     
-    var room_number = socket.room
-    console.log('receive a message: ' + message)
-    console.log('room_number: ' + room_number)
+    var room_number = socket.room;
+    console.log('receive a message: ' + message);
+    console.log('room_number: ' + room_number);
 
-    io.in(room_number).emit('room_message', message);
+    //var baseUrl = 'http://localhost:3000/api/v1';
+    var baseUrl = 'https://simple-chat-rails-server.herokuapp.com/api/v1';
+    var path = baseUrl + '/users/' + senderId + "/rooms/" + room_number + "/messages";
+
+    request.post(path, {form: {content: message}}, function(error, response, body) {
+      
+      if(!error && response.statusCode == 201) {
+        console.log("success to create message")
+        io.in(room_number).emit('room_message', senderId, message);
+
+
+        // Notification User
+        var rooms = io.sockets.adapter.rooms[room_number];
+        var roomMembers = [];
+        if (rooms) {
+          for (var id in rooms) {
+            roomMembers.push(io.sockets.adapter.nsp.connected[id].senderId);
+            console.log("fuck room");
+            console.log(roomMembers);
+            //console.log(s.senderId);
+          }
+        }
+
+
+        path = baseUrl + '/rooms/' + room_number + '/notification'
+        request.post(path, {form: {user_ids: roomMembers.toString()}}, function(error, response, body) {
+      
+          if(!error && response.statusCode == 201) {
+            console.log("success to notification message")
+            //io.in(room_number).emit('room_message', senderId, message);
+
+          } else {
+            console.log("error" + error)
+
+          }
+        });
+
+      } else {
+        console.log("error" + error)
+
+      }
+      //console.log(httpResponse);
+      //console.log(body);
+
+    });
+
+    //var clients = io.sockets.clients('room_number'); 
+    //console.log("clients" + clients );
+
+
+
+
+
+
+
+
+    
     //socket.room = room_number
   });
 
